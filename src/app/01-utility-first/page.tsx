0f518a4d80aa8ae96266567f856eedce28cc2fd5
Edwin Cardenas <eacardenase@unal.edu.co>
2023-11-07 15:53:26 -0500

    feat: utility first approach

--- src/app/01-utility-first/page.tsx
+++ src/app/01-utility-first/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Utility First Framework',
@@ -6,8 +7,20 @@ export const metadata: Metadata = {
 
 const UtilityFirst = () => {
   return (
-    <main>
-      <h1>Utility First</h1>
+    <main className='flex h-screen items-center justify-center'>
+      <div className='mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg'>
+        <Image
+          className='h-12 w-12'
+          src='/assets/img/warning.svg'
+          alt='warning'
+          width={20}
+          height={20}
+        />
+        <div>
+          <div className='text-xl font-medium text-black'>Are You Sure?</div>
+          <p className='text-slate-500'>You are about to delete a post</p>
+        </div>
+      </div>
     </main>
   );
 };
